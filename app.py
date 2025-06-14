0235096caf3b8ef375773df2b8b7f2b74e1cb24f
Bboy-Priyansh <61755193+Bboy-Priyansh@users.noreply.github.com>
2025-06-14 14:43:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello and welcom to FlaSH's CI/CD!"
+    return "Hello and welcome to FlaSH's CI/CD!"
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
